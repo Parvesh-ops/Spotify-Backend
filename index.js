ee278fd032dc14aba71e85fca1fd5d1c6f87918a
Parvesh <arveshchaudhary@gmail.com>
2026-02-17 19:38:54 +0545

    Setting up backend project

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express"
+import dotenv from 'dotenv'
+import cookieParser from "cookie-parser"
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+//middelware
+app.use(express.json());  // Parse JSON bodies for req.body in controllers
+app.use(cookieParser());  // cookieParser
+
+//test route
+app.get('/', (req, res) => {
+    res.status(201).json({ message: 'Hello from backend side' })
+})
+
+
+//server listen
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+
+})
